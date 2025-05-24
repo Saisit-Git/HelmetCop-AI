e7096472c81a8e25d6ae2405b316f047e2b70253
saisit <saisit.ia510747@gmail.com>
2025-05-25 00:45:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -264,7 +264,7 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=false)
     #app.run(debug=True)
     #http://localhost:8000/video for video source
     #http://localhost:8000 for image sourcep
